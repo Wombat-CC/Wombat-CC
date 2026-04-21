4a8aecad6973bd0da6b30f33812589f61351ec6c
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-04-20 22:23:45 -0400

    feat: add submodules for more features

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Wombat-CC/Arm.hpp>
 #include <Wombat-CC/Drivetrain.hpp>
 
-int main()
+int main() 
 {
     std::cout << "Welcome to your Wombat CC project (C++)\n";
     std::cout << "Using KIPR libwallaby v" << KIPR_VERSION << std::endl;
