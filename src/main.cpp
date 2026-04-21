a93068898fe1575c3df9dfb736faf62627373b63
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-04-20 22:18:33 -0400

    refactor: update C++ standard to C++26 and remove unused flake files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <kipr/wombat.h>
+#include <Wombat-CC/Arm.hpp>
+#include <Wombat-CC/Drivetrain.hpp>
+
+int main()
+{
+    std::cout << "Welcome to your Wombat CC project (C++)\n";
+    std::cout << "Using KIPR libwallaby v" << KIPR_VERSION << std::endl;
+
+    return 0;
+}
